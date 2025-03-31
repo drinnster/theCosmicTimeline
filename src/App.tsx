185e3181026011495d020e8e8f45bc74c0c20f63
drinnster <drinnable@gmail.com>
2025-03-31 20:05:11 +0300

    install and configure tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,13 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <h1>The Cosmic Timeline</h1>
+      <div class="mt-5">
+        <button class="rounded-full bg-gray-600 px-6 py-2">
+          Testing Tailwind
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </div>
     </>
   )
 }
