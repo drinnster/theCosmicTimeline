e6dfbd81ee93f0995737421b05f311ced2c70468
drinnster <drinnable@gmail.com>
2025-03-31 22:17:31 +0300

    cosmic colors for background and text

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ function App() {
   return (
     <>
       <div>
-        <h1>The Cosmic Timeline</h1>
+        <h1 class="text-supernovawhite">The Cosmic Timeline</h1>
       <div class="mt-5">
-        <button class="rounded-full bg-gray-600 px-6 py-2">
+        <button class="rounded-full bg-meteororange text-supernovawhite px-6 py-2">
           Testing Tailwind
         </button>
       </div>
